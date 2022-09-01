440c0a00894745707f23659e93afc6499c0aade4
jiangying <krivergo3@gmail.com>
2022-09-01 19:11:25 +0800

    fix typo

--- src/utils/nest_ref_parser.js
+++ src/utils/nest_ref_parser.js
@@ -4,21 +4,21 @@ import JSRP from "json-schema-ref-parser";
 export function dereference(json_obj, callback) {
     let parser = new JSRP();
 
-    inne_parse(parser, json_obj).then(function(){
+    inner_parse(parser, json_obj).then(function(){
         callback && callback(null, json_obj)
     }).catch(function(e){
         callback && callback(e)
     })
 }
 
-async function inne_parse(parser, json_obj) {
+async function inner_parse(parser, json_obj) {
     for (const [key, value] of Object.entries(json_obj)) {    
         if (key == "schema") {
             let schema = await parser.dereference(value)
             json_obj[key] = schema
         } else {
             if (Object.prototype.toString.call(value) === '[Object Object]') {
-                await inne_parse(parser, value)
+                await inner_parse(parser, value)
             }
         }
     }
